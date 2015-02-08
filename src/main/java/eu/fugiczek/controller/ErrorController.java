40753829cbb7a2a933e060ab09f8b3f982be443f
Fugiczek <dnovotnyd@gmail.com>
2015-02-08 17:28:40 +0100

    easy error pages handling

---
+++ src/main/java/eu/fugiczek/controller/ErrorController.java
@@ -0,0 +1,42 @@
+package eu.fugiczek.controller;
+
+import java.text.MessageFormat;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorController {
+
+	@RequestMapping
+	public String error(HttpServletRequest request, HttpServletResponse response, Model model) {
+		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+		String exceptionMessage = getExceptionMessage(throwable, statusCode);
+
+		String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
+		if (requestUri == null) {
+			requestUri = "Unknown";
+		}
+
+		String message = MessageFormat.format("{0} returned for {1} with message {2}", statusCode, requestUri, exceptionMessage);
+
+		model.addAttribute("errorMessage", message);
+		return "index-error";
+	}
+
+	private String getExceptionMessage(Throwable throwable, Integer statusCode) {
+		if (throwable != null) {
+			return throwable.getClass().getName();
+		}
+		HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+		return httpStatus.getReasonPhrase();
+	}
+
+}
